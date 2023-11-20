847a47bcb123eef34c40d40ea3b65d0c6b5368f9
lucasfrias-dev <lucas.frias.ing@gmail.com>
2023-11-20 15:18:03 -0300

    Create Material.java

---
+++ src/main/java/ar/edu/unnoba/appweb_concurso_materiales_educativos/model/Material.java
@@ -0,0 +1,59 @@
+package ar.edu.unnoba.appweb_concurso_materiales_educativos.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import jakarta.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name="materiales")
+public class Material implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_material", nullable = false)
+    private Long id;
+
+    @Column(name = "titulo")
+    @NotBlank(message = "El titulo es obligatorio")
+    private String titulo;
+
+    @NotBlank(message = "La descripcion es obligatoria")
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @NotBlank(message = "El tipo de material es obligatorio")
+    @Column(name = "tipo_material")
+    private String tipoMaterial;
+
+    @NotBlank(message = "La disciplina es obligatoria")
+    @Column(name = "disciplina")
+    private String disciplina;
+
+    @NotBlank(message = "El link es obligatorio")
+    @Column(name = "autores")
+    private String autores;
+
+    @Column(name = "aprobado")
+    private Boolean aprobado;
+
+    @Column(name = "evaluado")
+    private Boolean evaluado;
+
+    @Column(name = "likes")
+    private int likes = 0;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id")
+    private User concursante;
+
+    @ManyToMany(mappedBy = "materialesAEvaluar")
+    private Set<User> evaluadores = new HashSet<>();
+
+    @OneToMany(mappedBy = "material")
+    private Set<Evaluacion> evaluaciones = new HashSet<>();;
+}
