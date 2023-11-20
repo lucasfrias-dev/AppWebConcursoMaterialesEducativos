48e07fad049d7e81f3f1bc5af977a51254222f89
lucasfrias-dev <lucas.frias.ing@gmail.com>
2023-11-20 15:18:15 -0300

    Create MaterialRepository.java

---
+++ src/main/java/ar/edu/unnoba/appweb_concurso_materiales_educativos/repository/MaterialRepository.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.appweb_concurso_materiales_educativos.repository;
+
+import ar.edu.unnoba.appweb_concurso_materiales_educativos.model.Material;
+import ar.edu.unnoba.appweb_concurso_materiales_educativos.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MaterialRepository extends JpaRepository<Material, Long> {
+    List<Material> findByAprobadoTrue();
+
+    List<Material> findByConcursante(User user);
+}
