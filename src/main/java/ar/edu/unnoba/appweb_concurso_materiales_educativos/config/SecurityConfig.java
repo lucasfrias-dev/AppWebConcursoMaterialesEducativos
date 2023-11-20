33373aed127beab7f8101c3382a2f6e0983fb801
TomasE <tesparza@unnoba.edu.ar>
2023-11-20 16:24:02 -0300

    hacer redireccionamiento para los paneles de admin o de concursante segun convenga

--- src/main/java/ar/edu/unnoba/appweb_concurso_materiales_educativos/config/SecurityConfig.java
+++ src/main/java/ar/edu/unnoba/appweb_concurso_materiales_educativos/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package ar.edu.unnoba.appweb_concurso_materiales_educativos.config;
 
-import ar.edu.unnoba.appweb_concurso_materiales_educativos.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Autowired
-    UserService userService;
+    UserDetailsService userService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
