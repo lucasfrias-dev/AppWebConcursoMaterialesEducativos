c732897fa28c8178a18ddd2c6b8eb2ee8292e28b
lucasfrias-dev <lucas.frias.ing@gmail.com>
2023-11-22 15:08:16 -0300

    Create EvaluadorController.java

---
+++ src/main/java/ar/edu/unnoba/appweb_concurso_materiales_educativos/controller/EvaluadorController.java
@@ -0,0 +1,46 @@
+package ar.edu.unnoba.appweb_concurso_materiales_educativos.controller;
+
+
+import ar.edu.unnoba.appweb_concurso_materiales_educativos.model.Material;
+import ar.edu.unnoba.appweb_concurso_materiales_educativos.model.User;
+import ar.edu.unnoba.appweb_concurso_materiales_educativos.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/evaluador")
+@PreAuthorize("hasRole('ROLE_EVALUADOR')")
+public class EvaluadorController {
+
+    @Autowired
+    private UserService userService;
+
+    //Esta es la pantalla de inicio o index//
+    @GetMapping("/")
+    public String userInSession(Authentication authentication, Model model) {
+        User usuario= (User) authentication.getPrincipal();
+        model.addAttribute("usuario", usuario);
+        return "evaluador/panel-evaluador";
+    }
+
+    /*Cerrar sesion*/
+    @GetMapping("/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
+    }
+}
