7e892bc547b0333ad38cb0a51cecdc29e0e98970
lucasfrias-dev <lucas.frias.ing@gmail.com>
2023-11-29 13:58:59 -0300

    Update EvaluadorController.java
    Se agregaron los metodos del controlador del evaluador

--- src/main/java/ar/edu/unnoba/appweb_concurso_materiales_educativos/controller/EvaluadorController.java
+++ src/main/java/ar/edu/unnoba/appweb_concurso_materiales_educativos/controller/EvaluadorController.java
@@ -1,8 +1,11 @@
 package ar.edu.unnoba.appweb_concurso_materiales_educativos.controller;
 
 
+import ar.edu.unnoba.appweb_concurso_materiales_educativos.model.Evaluacion;
 import ar.edu.unnoba.appweb_concurso_materiales_educativos.model.Material;
 import ar.edu.unnoba.appweb_concurso_materiales_educativos.model.User;
+import ar.edu.unnoba.appweb_concurso_materiales_educativos.service.EvaluacionService;
+import ar.edu.unnoba.appweb_concurso_materiales_educativos.service.MaterialService;
 import ar.edu.unnoba.appweb_concurso_materiales_educativos.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,10 +16,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/evaluador")
@@ -24,12 +30,18 @@ import java.util.List;
 public class EvaluadorController {
 
     @Autowired
+    private MaterialService materialService;
+
+    @Autowired
     private UserService userService;
 
+    @Autowired
+    private EvaluacionService evaluacionService;
+
     //Esta es la pantalla de inicio o index//
-    @GetMapping("/")
+    @GetMapping("/index")
     public String userInSession(Authentication authentication, Model model) {
-        User usuario= (User) authentication.getPrincipal();
+        User usuario = (User) authentication.getPrincipal();
         model.addAttribute("usuario", usuario);
         return "evaluador/panel-evaluador";
     }
@@ -42,5 +54,44 @@ public class EvaluadorController {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
         return "redirect:/login?logout";
+    }
+
+    //metodo para que el evaluador pueda ver sus materiales asignados
+    @GetMapping("/materiales/asignados")
+    public String showMaterialesAsignados(Authentication authentication, Model model) {
+        User usuario = (User) authentication.getPrincipal();
+        model.addAttribute("usuario", usuario);
+        Set<Material> materiales = materialService.getMaterialesAsignados(usuario);
+        List<Boolean> fueEvaluado = materiales.stream()
+                .map(material -> userService.haEvaluadoMaterial(usuario, material))
+                .collect(Collectors.toList());
+        model.addAttribute("materiales", materiales);
+        model.addAttribute("hasUserEvaluated", fueEvaluado);
+        return "evaluador/materiales-asignados";
+    }
+
+    @GetMapping("/materiales/asignados/{id}")
+    public String showDetalleMaterial(@PathVariable Long id, Model model, Authentication authentication){
+        User usuario = (User) authentication.getPrincipal();
+        Material material = materialService.getMaterial(id);
+        model.addAttribute("material", material);
+        boolean haEvaluado = userService.haEvaluadoMaterial(usuario, material);
+        model.addAttribute("haEvaluado", haEvaluado);
+        if (haEvaluado) {
+            Evaluacion evaluacion = evaluacionService.getEvaluacionByUserAndMaterial(usuario, material);
+            model.addAttribute("evaluacion", evaluacion);
+        } else {
+            model.addAttribute("evaluacion", new Evaluacion());
+        }
+        return "evaluador/detalle-material";
+    }
+
+    @PostMapping("/materiales/asignados/{id}/createEvaluacion")
+    public String createEvaluacion(@PathVariable Long id, @ModelAttribute Evaluacion evaluacion, Authentication authentication) {
+        User evaluador = (User) authentication.getPrincipal();
+        Material material = materialService.getMaterial(id);
+        evaluacionService.createEvaluacion(evaluacion, evaluador, material);
+        materialService.estaEvaluado(material);
+        return "redirect:/evaluador/materiales/asignados";
     }
 }
