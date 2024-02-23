5b4561c73e0ba09026883efe03987cdb84eb7ca2
lucasfrias-dev <lucas.frias.ing@gmail.com>
2024-02-23 15:57:37 -0300

    Create MaterialDTO.java

---
+++ src/main/java/ar/edu/unnoba/appweb_concurso_materiales_educativos/dto/MaterialDTO.java
@@ -0,0 +1,19 @@
+package ar.edu.unnoba.appweb_concurso_materiales_educativos.dto;
+
+import ar.edu.unnoba.appweb_concurso_materiales_educativos.model.User;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class MaterialDTO implements Serializable {
+    private Long id;
+    private String titulo;
+    private String descripcion;
+    private String tipoMaterial;
+    private String disciplina;
+    private String autores;
+    private User concursante;
+
+}
+
